3e45d324f037ad7a35da4d467a0258c3d692a3f0
matias <matiasgonzaloacosta@gmail.com>
2020-03-29 16:09:24 -0300

    Updates

---
+++ precache-manifest.2ebe4c58fca938713fa6ca2840430ebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7238d5ec303bbe907055cadef2123340",
+    "url": "/management-patients-pets/index.html"
+  },
+  {
+    "revision": "4816530177d5bf704725",
+    "url": "/management-patients-pets/static/css/main.506ca484.chunk.css"
+  },
+  {
+    "revision": "ddd32b7e7a590af9c795",
+    "url": "/management-patients-pets/static/js/2.af810f6c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/management-patients-pets/static/js/2.af810f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4816530177d5bf704725",
+    "url": "/management-patients-pets/static/js/main.619dc2c4.chunk.js"
+  },
+  {
+    "revision": "c0b9d215a1595b74c4af",
+    "url": "/management-patients-pets/static/js/runtime-main.831bfb5c.js"
+  },
+  {
+    "revision": "f19f932f736d49976e00cf944887f112",
+    "url": "/management-patients-pets/static/media/green-tree-python.f19f932f.jpg"
+  }
+]);
